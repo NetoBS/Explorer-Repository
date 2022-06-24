a27e6d261b330e8332f3d01e6680b7c64ad21309
Neto Sousa <netosousa29@yahoo.com>
2022-06-24 15:25:45 -0300

    Estilização e Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useRouteMatch, Link } from 'react-router-dom'
-import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 import logoImg from '../../assets/logo.svg';
-import { Header } from './style';
+import { Header, RepositoryInfo, Issues } from './style';
 
 
 interface RepositoryParams {
@@ -14,13 +14,51 @@ const Repository: React.FC = () => {
     const  { params }  =  useRouteMatch<RepositoryParams>();
 
     return (
-        <Header>
-            <img src={logoImg} alt="Git Explorer" />
-            <Link to="/dashboard">
-                <FiChevronLeft size={16}/>
-                voltar
-            </Link>
-        </Header>
+        <>
+            <Header>
+                <img src={logoImg} alt="Git Explorer" />
+                <a href="/">
+                    <FiChevronLeft size={16}/>
+                    voltar
+                </a>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars.githubusercontent.com/u/105009963?v=4" alt="NetoBS"/>
+                    <div>
+                        <strong>NetoBS/AppGoBarber</strong>
+                        <p>descrição do repositório</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <a  href={"addsadas"}>
+
+                    <div>
+                        <strong>asdsadas</strong>
+                        <p>asdsadsad</p>
+                    </div>
+
+                    <FiChevronRight size={20}/>
+                </a>
+            </Issues>
+        </>
     )
 };
 
